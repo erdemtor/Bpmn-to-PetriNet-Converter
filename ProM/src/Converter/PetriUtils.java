db07c49b0ce6e75cacdbe22794264d7ca6dab686
Erdem Toraman <erdemtoraman93@gmail.com>
2016-11-03 18:26:28 +0200

    *

---
+++ ProM/src/Converter/PetriUtils.java
@@ -0,0 +1,93 @@
+package Converter;
+
+import BPMN.*;
+import Petri.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by Erdem on 03-Nov-16.
+ */
+public class PetriUtils {
+
+    public static void appendToPetri(Petri result, List<Petri> ptrList, String splitType) {
+        if (splitType.equals("xor-split")) {
+            for(Petri tempPetri : ptrList){
+                getLastPlace(result).getOutgoingTransitions().addAll(tempPetri.firstPlace().getOutgoingTransitions());
+            }
+            Place joinPlace = new Place("JoinPlace","", result);
+            for(Petri tempPetri : ptrList){
+                result.getPlaces().addAll(tempPetri.getPlaces());
+                result.getTransitions().addAll(tempPetri.getTransitions());
+                removeLastPlace(tempPetri);
+                getLastTransition(tempPetri).getTargetPlaces().add(joinPlace);
+            }
+        }
+        if (splitType.equals("and-split")) {
+            Transition andSplitTransition = new Transition("ANDSPLIT",result);
+            getLastPlace(result).getOutgoingTransitions().add(andSplitTransition);
+            for(Petri subPetri : ptrList){
+                andSplitTransition.getTargetPlaces().add(subPetri.firstPlace());
+            }
+            Transition joinTransition = new Transition("ANDJOIN",result);
+            Place afterJoin = new Place("afterJoin","",result);
+            joinTransition.getTargetPlaces().add(afterJoin);
+            for(Petri subPetri : ptrList){
+                result.getPlaces().addAll(subPetri.getPlaces());
+                result.getTransitions().addAll(subPetri.getTransitions());
+                getLastPlace(subPetri).getOutgoingTransitions().add(joinTransition);
+            }
+        }
+    }
+
+    public static void convertAndAdd(Petri petri, Node current) {
+        if(current == null) return;
+        if (current instanceof Compound) {
+            Petri smallPetri = Converter.convert(((Compound) current).getSubBpmn());
+            petri.setPlaces(Stream.concat(petri.getPlaces().stream(), smallPetri.getPlaces().stream()).collect(Collectors.toList()));
+            petri.setTransitions(Stream.concat(petri.getTransitions().stream(), smallPetri.getTransitions().stream()).collect(Collectors.toList()));
+            Transition connector = new Transition("connector", petri);
+            connector.getTargetPlaces().add(smallPetri.firstPlace());
+            getLastPlace(petri).getOutgoingTransitions().add(connector);
+            petri.getTransitions().add(connector);
+        } else {
+            Transition tra = new Transition(current.toString(),petri);
+            Place plc = new Place(current.toString(),"", petri);
+            tra.getTargetPlaces().add(plc);
+            getLastPlace(petri).getOutgoingTransitions().add(tra);
+        }
+    }
+    public static Place getStartPlace(Petri petri) {
+        for(Place plc : petri.getPlaces()) {
+            if (plc.getType().equals("start")){
+                return plc;
+            }
+        }
+        return null;
+    }
+    public static Place getLastPlace(Petri petri){
+        for(Place plc : petri.getPlaces()) {
+            if (plc.getOutgoingTransitions().size() == 0)	return plc;
+        }
+        return null;
+    }
+
+    public static Transition getLastTransition(Petri petri){
+        for(Transition tra : petri.getTransitions()) {
+            if (tra.getTargetPlaces().size() == 0)	return tra;
+        }
+        return null;
+    }
+
+    public static void removeLastPlace(Petri petri){
+        Place last = getLastPlace(petri);
+        petri.getPlaces().remove(last);
+        for(Transition tra : petri.getTransitions()) {
+            if (tra.getTargetPlaces().contains(last)) {
+                tra.getTargetPlaces().remove(last);
+            }
+        }
+    }
+}
